e4edcdc6d8924716e48020f118ac5f1335948d4f
Adam Fowler <adam@adamfowler.org>
2020-08-05 09:53:31 +0100

    added c19x

---
+++ app/src/main/java/org/c19x/MainActivity.java
@@ -0,0 +1,16 @@
+package org.c19x;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.c19x.sensor.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
